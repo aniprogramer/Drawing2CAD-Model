3b3c52effbeec6168625667302915f2b0747b1e9
Ani Programmer <aniketde66@gmail.com>
2026-03-30 23:57:43 +0530

    made changes suitable for 3050 GPU

--- config/config.py
+++ config/config.py
@@ -78,8 +78,8 @@ class Config(object):
         parser.add_argument('--exp_name', type=str, default=os.getcwd().split('/')[-1], help="name of this experiment")
         parser.add_argument('-g', '--gpu_ids', type=str, default='0', help="gpu to use, e.g. 0  0,1,2. CPU not supported.")        
         
-        parser.add_argument('--batch_size', type=int, default=256, help="batch size")
-        parser.add_argument('--num_workers', type=int, default=8, help="number of workers for data loading")
+        parser.add_argument('--batch_size', type=int, default=8, help="batch size")
+        parser.add_argument('--num_workers', type=int, default=0, help="number of workers for data loading")
 
         parser.add_argument('--nr_epochs', type=int, default=200, help="total number of epochs to train")
         parser.add_argument('--lr', type=float, default=1e-3, help="initial learning rate")
